dbf5c8a915310968e8973e0a02048f1e45886716
gregorymolero <gregory.molero@gmail.com>
2015-08-06 13:17:13 +0200

    tp3

--- 2-phpbasic/imc.php
+++ 2-phpbasic/imc.php
@@ -10,20 +10,36 @@
         Entrez votre poids: <input type="float" name="poids"/><br>
         <input type="submit" name="valid" value="Calculer"/>
         </form>
-    </body>
-</html>
 <?php
 if (isset($_POST['valid'])) {
     $prénom=$_POST['prénom'];
-    $taille=$_POST[taille];
+    $taille=$_POST['taille'];
     $poids=$_POST['poids'];
 }
-$imc=$poids/($taille*$taille)
+$imc=$poids/($taille*$taille);
+echo 'Bonjour '.$prénom.'votre IMC est de : '.$imc.'.<br>';
 if ($imc<16.5){
-    $verdict='vous etes en dénutrition';
+    $verdict='Vous etes en dénutrition.';
+}
+elseif ($imc<18.5) {
+    $verdict='Vous etes maigre.';
+}
+elseif ($imc<25){
+    $verdict='Vous etes de corpulence normale.';
+}
+elseif (imc<30){
+    $verdict='Vous etes en surpoids.';
+}
+elseif ($imc<35){
+    $verdict='Vous êtes modérement obèse.';
+}
+elseif ($imc<40){
+    $verdict='Vous soufferez d\'obésité sévère.';
 }
 else {
-    $verdict='ca va pour vous';
+    $verdict='Vous souferez d\'obésité morbide.';
 }
 echo $verdict
 ?>
+    </body>
+</html>
